b025978f13b585a0d7710d08e223daa9504b0c84
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-09-28 20:33:15 +0100

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,25 +1,30 @@
-let badwordRegex = /fuck|shit|dick|pussy|bitch|tits|boobs|bastard|asshole|slut|whore|cunt|nigga|faggot|cock|dildo|prick|wanker|jerkoff|motherfucker|retard|idiot|loser/i // tambahin sendiri
+// Anti-toxic / Badword filter plugin
+
+// Regex list of banned words
+const badwordRegex = /fuck|shit|dick|pussy|bitch|tits|boobs|bastard|asshole|slut|whore|cunt|nigga|faggot|cock|dildo|prick|wanker|jerkoff|motherfucker|retard|idiot|loser/i
 
 export function before(m, { isBotAdmin }) {
-    if (m.isBaileys && m.fromMe) return !0
-    let chat = global.db.data.chats[m.chat]
-    let user = global.db.data.users[m.sender]
-    let isBadword = badwordRegex.exec(m.text)
-    console.log(isBadword)
+    if (m.isBaileys && m.fromMe) return true
+
+    let chat = global.db.data.chats[m.chat] || {}
+    let user = global.db.data.users[m.sender] || {}
+    let isBadword = badwordRegex.test(m.text)
 
     if (chat.antiBadword && isBadword) {
+        if (!user.warning) user.warning = 0
         user.warning += 1
-        m.reply('Don't be toxic, okay!!\n' + `you have ${user.warning} warning` + '\nTo turn off type *.disable antibadword*')
+
+        m.reply(
+            `🚫 Don't be toxic!\nYou now have ${user.warning} warning(s).\nTo turn it off, type *.disable antibadword*`
+        )
+
         if (user.warning >= 5) {
-            user.banned = false
+            user.banned = true
             user.warning = 0
-            if (m.isGroup) {
-                if (isBotAdmin) {
-                    this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
-                    //this.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
-                }
+            if (m.isGroup && isBotAdmin) {
+                this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
             }
         }
     }
-    return !0
+    return true
 }
